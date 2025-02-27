40373bbca0e3858a8ca98a300a63390b41ec5883
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2025-02-27 03:40:57 +0330

    Commited

---
+++ SsoSample.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SsoSample.MVC.Models;
+
+namespace SsoSample.console;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        
+        client.BaseAddress = new Uri("https://localhost:7001");
+
+        var stringResult = await client.GetStringAsync("/WeatherForecast");
+
+        var result = JsonConvert.DeserializeObject<List<WeatherForecast>>(stringResult);
+
+        foreach (var item in result!) {
+
+            Console.WriteLine($"{item.Date}\t {item.Summery}\t {item.TemperatureC}\t {item.TemperatureF}");
+            Console.WriteLine("".PadLeft(200, '-'));
+        }
+        Console.ReadLine();
+    }
+}
